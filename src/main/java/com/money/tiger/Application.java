1c7fb423a12e159094130600c6db0136b3caa767
zhuhongyu345 <2352539448@qq.com>
2025-05-05 00:55:11 +0800

    spring basic import

---
+++ src/main/java/com/money/tiger/Application.java
@@ -0,0 +1,13 @@
+package com.money.tiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
