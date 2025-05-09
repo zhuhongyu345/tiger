e1b31543b6f3c22b53cfe922978234ef193d6d46
zhuhongyu345 <jdk_18@163.com>
2025-05-09 10:57:13 +0800

    升级mongodb版本

--- src/main/java/com/money/tiger/dao/config/EmbeddedMongoConfig.java
+++ src/main/java/com/money/tiger/dao/config/EmbeddedMongoConfig.java
@@ -1,13 +1,14 @@
 package com.money.tiger.dao.config;
 
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.config.Storage;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.embed.mongo.types.DatabaseDir;
+import de.flapdoodle.reverse.StateID;
+import de.flapdoodle.reverse.TransitionWalker;
+import de.flapdoodle.reverse.Transitions;
+import de.flapdoodle.reverse.transitions.Start;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -16,6 +17,10 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProce
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class EmbeddedMongoConfig {
@@ -32,13 +37,31 @@ public class EmbeddedMongoConfig {
                 try {
                     Environment env = beanFactory.getBean(Environment.class);
                     Integer port = env.getProperty("spring.data.mongodb.port", Integer.class);
-                    IMongodConfig config = new MongodConfigBuilder()
-                            .version(Version.Main.V4_0)
-                            .net(new Net("localhost", port == null ? 27018 : port, Network.localhostIsIPv6()))
-                            .replication(new Storage("mongo-data", null, 0))
-                            .build();
-                    MongodExecutable executable = MongodStarter.getDefaultInstance().prepare(config);
-                    executable.start();
+                    Version version = Version.V8_0_0;
+                    Path path = Paths.get("mongo-data-" + version.toString().replace(".", "")
+                            .replace("Version{", "").replace("}", ""));
+                    File file = new File(path.toUri());
+                    if (!file.exists()) {
+                        file.mkdirs();
+                    }
+                    System.out.println(file.getAbsolutePath());
+
+                    Transitions transitions = Mongod.instance()
+                            .withNet(Start.to(Net.class).initializedWith(Net.defaults().withPort(port)))
+                            .withDatabaseDir(Start.to(DatabaseDir.class).initializedWith(DatabaseDir.of(path)))
+                            .transitions(version);
+                    TransitionWalker.ReachedState<RunningMongodProcess> executable = transitions.walker()
+                            .initState(StateID.of(RunningMongodProcess.class));
+
+
+                    //old
+//                    IMongodConfig config = new MongodConfigBuilder()
+//                            .version(Version.Main.V4_0)
+//                            .net(new Net("localhost", port == null ? 27018 : port, Network.localhostIsIPv6()))
+//                            .replication(new Storage("mongo-data", null, 0))
+//                            .build();
+//                    MongodExecutable executable = MongodStarter.getDefaultInstance().prepare(config);
+//                    executable.start();
                     beanFactory.registerSingleton("embeddedMongoServer", executable);
                 } catch (Exception e) {
                     throw new BeanCreationException("Failed to start embedded MongoDB", e);
